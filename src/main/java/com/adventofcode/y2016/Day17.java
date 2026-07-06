2c96df307545ae22785c6cb16b90b05101a7f57d
slawekgrzegorzewski <slawek.grzegorzewski@satago.com>
2026-07-06 23:31:33 +0200

    Y2016 day17

--- src/main/java/com/adventofcode/y2016/Day17.java
+++ src/main/java/com/adventofcode/y2016/Day17.java
@@ -1,40 +1,106 @@
 package com.adventofcode.y2016;
 
 import com.adventofcode.y2016.input.Input;
+import org.jspecify.annotations.NonNull;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class Day17 {
 
     private static final char[] HEX_DIGITS = "0123456789abcdef".toCharArray();
+    private static final int UP_INDEX = 0;
+    private static final int DOWN_INDEX = 1;
+    private static final int LEFT_INDEX = 2;
+    private static final int RIGHT_INDEX = 3;
+    private final Set<Character> OPEN_DOOR_CHARS = Set.of('b', 'c', 'd', 'e', 'f');
     private final MessageDigest md = MessageDigest.getInstance("MD5");
     private final String passcode;
-
+    private final String[] shortestAndLongestPath;
 
     public Day17() throws IOException, NoSuchAlgorithmException {
         this.passcode = Input.day17();
+        shortestAndLongestPath = work();
     }
 
-    long part1() {
-        return 0L;
+    String[] work() {
+        List<Node> nodes = new ArrayList<>();
+        Node startNode = new Node("", UP_INDEX, UP_INDEX, new ArrayList<>());
+        nodes.add(startNode);
+        return walk(nodes, startNode, null, null);
+    }
+
+    private String[] walk(List<Node> nodes, Node node, String minSolution, String maxSolution) {
+        if (node.x == 3 && node.y == 3) {
+            return compileResult(node, minSolution, maxSolution);
+        }
+        List<Node> newNodes = new ArrayList<>();
+        char[] openDoorsMarker = firstFourCharsOfMd5(passcode + node.path, md);
+        if (OPEN_DOOR_CHARS.contains(openDoorsMarker[UP_INDEX]) && node.y > 0) {
+            Node newNode = new Node(node.path + "U", node.x, node.y - 1, new ArrayList<>());
+            newNodes.add(newNode);
+        }
+        if (OPEN_DOOR_CHARS.contains(openDoorsMarker[DOWN_INDEX]) && node.y < 3) {
+            Node newNode = new Node(node.path + "D", node.x, node.y + 1, new ArrayList<>());
+            newNodes.add(newNode);
+        }
+        if (OPEN_DOOR_CHARS.contains(openDoorsMarker[LEFT_INDEX]) && node.x > 0) {
+            Node newNode = new Node(node.path + "L", node.x - 1, node.y, new ArrayList<>());
+            newNodes.add(newNode);
+        }
+        if (OPEN_DOOR_CHARS.contains(openDoorsMarker[RIGHT_INDEX]) && node.x < 3) {
+            Node newNode = new Node(node.path + "R", node.x + 1, node.y, new ArrayList<>());
+            newNodes.add(newNode);
+        }
+        nodes.addAll(newNodes);
+        node.children.addAll(newNodes);
+        for (Node newNode : newNodes) {
+            String[] s = walk(nodes, newNode, minSolution, maxSolution);
+            if (s[0] != null && (minSolution == null || minSolution.length() > s[0].length())) {
+                minSolution = s[0];
+            }
+            if (s[1] != null && (maxSolution == null || maxSolution.length() < s[1].length())) {
+                maxSolution = s[1];
+            }
+        }
+        return new String[]{minSolution, maxSolution};
+    }
+
+    private static String @NonNull [] compileResult(Node node, String minSolution, String maxSolution) {
+        return new String[]{
+                minSolution == null || minSolution.length() > node.path.length()
+                        ? node.path
+                        : minSolution,
+                maxSolution == null || maxSolution.length() < node.path.length()
+                        ? node.path
+                        : maxSolution};
+    }
+
+    String part1() {
+        return shortestAndLongestPath[0];
     }
 
     long part2() {
-        return 0L;
+        return shortestAndLongestPath[1].length();
     }
 
-    private String md5(String value, MessageDigest md) {
+    private record Node(String path, int x, int y, List<Node> children) {
+    }
+
+    private char[] firstFourCharsOfMd5(String value, MessageDigest md) {
         md.reset();
         byte[] hash = md.digest(value.getBytes(StandardCharsets.UTF_8));
-        char[] hex = new char[hash.length * 2];
-        for (int i = 0, j = 0; i < hash.length; i++) {
+        char[] hex = new char[4];
+        for (int i = UP_INDEX, j = UP_INDEX; j < 4; i++) {
             int b = hash[i] & 0xff;
             hex[j++] = HEX_DIGITS[b >>> 4];
             hex[j++] = HEX_DIGITS[b & 0x0f];
         }
-        return new String(hex);
+        return hex;
     }
 }
--- src/test/java/com/adventofcode/y2016/Day17Test.java
+++ src/test/java/com/adventofcode/y2016/Day17Test.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,17 +14,17 @@ class Day17Test {
     static Day17 day17;
 
     @BeforeAll
-    public static void init() throws IOException {
+    public static void init() throws IOException, NoSuchAlgorithmException {
         day17 = new Day17();
     }
 
     @Test
     void testPart1() {
-        assertEquals(0L, day17.part1());
+        assertEquals("RLDUDRDDRR", day17.part1());
     }
 
     @Test
     void testPart2() {
-        assertEquals(0L, day17.part2());
+        assertEquals(590L, day17.part2());
     }
 }
