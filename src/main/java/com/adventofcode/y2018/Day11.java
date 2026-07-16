60f753b9d6e77207ec471655bd25cfc3cf7011a6
Sławek <slawek.grz@gmail.com>
2026-07-16 20:54:57 +0200

    y2018 day11

--- src/main/java/com/adventofcode/y2018/Day11.java
+++ src/main/java/com/adventofcode/y2018/Day11.java
@@ -12,53 +12,75 @@ public class Day11 {
         this.serialNumber = Input.day11();
     }
 
-    private void print(int[][] ints) {
-        for (int[] ints1 : ints) {
-            for (int anInt : ints1) {
-                System.out.print(anInt + "\t");
-            }
-            System.out.println();
-        }
-    }
-
     String part1() {
-        SquareIdentifierWithPower squareIdentifierWithPower = findMaxPowerSquareOfSize(initMap(), 3);
-        return squareIdentifierWithPower.squareIdentifier().x() + "," + squareIdentifierWithPower.squareIdentifier().y();
+        int[][] map = initMap();
+        int[][] partialSums = copy(map);
+        findMaxPowerSquareOfSize(map, partialSums, 2);
+        return findMaxPowerSquareOfSize(map, partialSums, 3).toPart1String();
     }
 
     String part2() {
         int[][] map = initMap();
+        int[][] partialSums = copy(map);
         SquareIdentifierWithPower maxPower = new SquareIdentifierWithPower(new SquareIdentifier(0, 0, 0), Integer.MIN_VALUE);
         for (int size = 1; size <= 300; size++) {
-            SquareIdentifierWithPower squareIdentifierWithPower = findMaxPowerSquareOfSize(map, size);
+            SquareIdentifierWithPower squareIdentifierWithPower = findMaxPowerSquareOfSize(map, partialSums, size);
             if (squareIdentifierWithPower.power() > maxPower.power()) {
                 maxPower = squareIdentifierWithPower;
             }
-            if (squareIdentifierWithPower.power() < 0) break;
         }
-        return maxPower.squareIdentifier().x() + "," + maxPower.squareIdentifier().y() + "," + maxPower.squareIdentifier().size();
+        return maxPower.toPart2String();
     }
 
     private int[][] initMap() {
         int[][] map = new int[300][300];
         for (int x = 1; x <= 300; x++) {
             for (int y = 1; y <= 300; y++) {
-                map[y - 1][x - 1] = (((x + 10) * y + serialNumber) * (x + 10)) / 100 % 10 - 5;
+                map[y - 1][x - 1] = calculatePower(x, y);
             }
         }
         return map;
     }
 
+    private int calculatePower(int x, int y) {
+        int rackId = x + 10;
+        int power = rackId * y + serialNumber;
+        power *= rackId;
+        power = (power / 100) % 10;
+        power -= 5;
+        return power;
+    }
 
-    private static SquareIdentifierWithPower findMaxPowerSquareOfSize(int[][] map, int size) {
+    private int[][] copy(int[][] map) {
+        int[][] copy = new int[map.length][map[0].length];
+        for (int i = 0; i < map.length; i++) {
+            System.arraycopy(map[i], 0, copy[i], 0, map[i].length);
+        }
+        return copy;
+    }
+
+
+    private static SquareIdentifierWithPower findMaxPowerSquareOfSize(int[][] map, int[][] partialSums, int size) {
         int maxPower = Integer.MIN_VALUE;
         int maxX = 0, maxY = 0;
-        for (int x = 0; x < 300 - size; x++) {
-            for (int y = 0; y < 300 - size; y++) {
-                int sum = 0;
-                for (int xOffset = 0; xOffset < size; xOffset++)
-                    for (int yOffset = 0; yOffset < size; yOffset++)
-                        sum += map[y + yOffset][x + xOffset];
+        int upperBound = 300 - size;
+        for (int y = 0; y <= upperBound; y++) {
+            int[] partialSumRow = partialSums[y];
+            int sum = 0;
+            for (int x = 0; x <= upperBound; x++) {
+                if (x == 0) {
+                    for (int xOffset = 0; xOffset < size; xOffset++) {
+                        if (size > 1)
+                            partialSumRow[xOffset] += map[y + size - 1][xOffset];
+                        sum += partialSumRow[xOffset];
+                    }
+                } else {
+                    int newColumn = x + size - 1;
+                    if (size > 1)
+                        partialSumRow[newColumn] += map[y + size - 1][newColumn];
+                    sum -= partialSumRow[x - 1];
+                    sum += partialSumRow[newColumn];
+                }
                 if (sum > maxPower) {
                     maxPower = sum;
                     maxX = x + 1;
@@ -73,5 +95,12 @@ public class Day11 {
     }
 
     public record SquareIdentifierWithPower(SquareIdentifier squareIdentifier, int power) {
+        public String toPart1String() {
+            return squareIdentifier.x() + "," + squareIdentifier.y();
+        }
+
+        public String toPart2String() {
+            return squareIdentifier.x() + "," + squareIdentifier.y() + "," + squareIdentifier.size();
+        }
     }
 }
