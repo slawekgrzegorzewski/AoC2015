204665932b68be787c314e0b40d5698ca05ce3b1
Sławek <slawek.grz@gmail.com>
2026-07-16 21:21:30 +0200

    y2018 day11

--- src/main/java/com/adventofcode/y2018/Day11.java
+++ src/main/java/com/adventofcode/y2018/Day11.java
@@ -13,18 +13,15 @@ public class Day11 {
     }
 
     String part1() {
-        int[][] map = initMap();
-        int[][] partialSums = copy(map);
-        findMaxPowerSquareOfSize(map, partialSums, 2);
-        return findMaxPowerSquareOfSize(map, partialSums, 3).toPart1String();
+        int[][] prefixes = initPrefixes(initMap());
+        return findMaxPowerSquareOfSize(prefixes, 3).toPart1String();
     }
 
     String part2() {
-        int[][] map = initMap();
-        int[][] partialSums = copy(map);
+        int[][] prefixes = initPrefixes(initMap());
         SquareIdentifierWithPower maxPower = new SquareIdentifierWithPower(new SquareIdentifier(0, 0, 0), Integer.MIN_VALUE);
         for (int size = 1; size <= 300; size++) {
-            SquareIdentifierWithPower squareIdentifierWithPower = findMaxPowerSquareOfSize(map, partialSums, size);
+            SquareIdentifierWithPower squareIdentifierWithPower = findMaxPowerSquareOfSize(prefixes, size);
             if (squareIdentifierWithPower.power() > maxPower.power()) {
                 maxPower = squareIdentifierWithPower;
             }
@@ -42,6 +39,24 @@ public class Day11 {
         return map;
     }
 
+    private int[][] initPrefixes(int[][] map) {
+        int[][] prefixes = new int[map.length][map[0].length];
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map.length; x++) {
+                if (x == 0 && y == 0) {
+                    prefixes[0][0] = map[0][0];
+                } else if (x == 0) {
+                    prefixes[y][0] = map[y][0] + prefixes[y - 1][0];
+                } else if (y == 0) {
+                    prefixes[0][x] = map[0][x] + prefixes[0][x - 1];
+                } else {
+                    prefixes[y][x] = map[y][x] + prefixes[y - 1][x] + prefixes[y][x - 1] - prefixes[y - 1][x - 1];
+                }
+            }
+        }
+        return prefixes;
+    }
+
     private int calculatePower(int x, int y) {
         int rackId = x + 10;
         int power = rackId * y + serialNumber;
@@ -51,36 +66,16 @@ public class Day11 {
         return power;
     }
 
-    private int[][] copy(int[][] map) {
-        int[][] copy = new int[map.length][map[0].length];
-        for (int i = 0; i < map.length; i++) {
-            System.arraycopy(map[i], 0, copy[i], 0, map[i].length);
-        }
-        return copy;
-    }
-
-
-    private static SquareIdentifierWithPower findMaxPowerSquareOfSize(int[][] map, int[][] partialSums, int size) {
+    private static SquareIdentifierWithPower findMaxPowerSquareOfSize(int[][] prefixes, int size) {
         int maxPower = Integer.MIN_VALUE;
         int maxX = 0, maxY = 0;
         int upperBound = 300 - size;
         for (int y = 0; y <= upperBound; y++) {
-            int[] partialSumRow = partialSums[y];
-            int sum = 0;
             for (int x = 0; x <= upperBound; x++) {
-                if (x == 0) {
-                    for (int xOffset = 0; xOffset < size; xOffset++) {
-                        if (size > 1)
-                            partialSumRow[xOffset] += map[y + size - 1][xOffset];
-                        sum += partialSumRow[xOffset];
-                    }
-                } else {
-                    int newColumn = x + size - 1;
-                    if (size > 1)
-                        partialSumRow[newColumn] += map[y + size - 1][newColumn];
-                    sum -= partialSumRow[x - 1];
-                    sum += partialSumRow[newColumn];
-                }
+                int sum = prefixes[y + size - 1][x + size - 1]
+                        - (x == 0 ? 0 : prefixes[y + size - 1][x - 1])
+                        - (y == 0 ? 0 : prefixes[y - 1][x + size - 1])
+                        + ((x == 0 || y == 0) ? 0 : prefixes[y - 1][x - 1]);
                 if (sum > maxPower) {
                     maxPower = sum;
                     maxX = x + 1;
